401a9021a6ad8455a53f281f4aec43e0fac7f4b8
maixhashi <maixhashi@gmail.com>
2024-10-14 03:14:22 +0900

    Feature_2024.10.14.03.13_Redux storeの設定(flowStepSlice / memberSlice)

--- src/resources/js/store/memberSlice.js
+++ src/resources/js/store/memberSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios'; // Axiosをインポート
 
+// メンバー追加のための非同期関数
 export const addMember = createAsyncThunk('members/addMember', async (newMember) => {
     const response = await axios.post('/api/members', newMember); // Axiosを使ってPOSTリクエスト
 
@@ -16,10 +17,11 @@ const memberSlice = createSlice({
     initialState: [],
     reducers: {
         setMembers: (state, action) => {
-            return action.payload;
+            return action.payload; // ステートを新しいメンバーリストで更新
         },
     },
 });
 
+// エクスポート
 export const { setMembers } = memberSlice.actions;
 export default memberSlice.reducer;
--- src/resources/js/store/store.js
+++ src/resources/js/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import memberReducer from './memberSlice';
 import flowstepsReducer from './flowstepsSlice';
+import flowStepReducer from './flowStepSlice'; // 適切なパスを指定
 
 
 const store = configureStore({
     reducer: {
         members: memberReducer,
-        flowsteps: flowstepsReducer
+        flowsteps: flowstepsReducer,
+        flowStep: flowStepReducer
     },
 });
 
