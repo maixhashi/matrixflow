3327e0152e52a445cb11513f00fe16ebd341d07d
maixhashi <maixhashi@gmail.com>
2024-10-16 05:54:07 +0900

    Refactore_2024.10.16.05.53_Sliceの削除(flowStepSlice / 重複Sliceの削除）

--- src/resources/js/store/store.js
+++ src/resources/js/store/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import memberReducer from './memberSlice';
 import flowstepsReducer from './flowstepsSlice';
-import flowStepReducer from './flowStepSlice'; // 適切なパスを指定
 
 
 const store = configureStore({
     reducer: {
         members: memberReducer,
         flowsteps: flowstepsReducer,
-        flowStep: flowStepReducer
     },
 });
 
