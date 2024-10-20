90032abd41cbdeb41d5ba72a1f9a837bcebcd7c1
maixhashi <maixhashi@gmail.com>
2024-10-20 11:45:17 +0900

    Feature_2024.10.20.11.45_ゲスト用のMatrixViewへのリンクを一時削除

--- src/resources/js/Components/Header.jsx
+++ src/resources/js/Components/Header.jsx
@@ -26,12 +26,12 @@ const Header = ({ auth }) => {
                     </Link>
                 ) : (
                     <>
-                        <Link
+                        {/* <Link
                             href={route('guest.matrixflow.create')}
                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
-                            マトリクスフローを作成してみる
-                        </Link>
+                            マトリクスフローを作成してみる(お試し用)
+                        </Link> */}
                         <Link
                             href={route('login')}
                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
