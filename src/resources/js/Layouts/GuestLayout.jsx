6c9f632b4e7f00c320ce3f4c0b7a6dd2e770f3c2
maixhashi <maixhashi@gmail.com>
2024-10-23 07:49:11 +0900

    Style_2024.10.23.07.48_コンポーネントの修正（GuestLayout / ログイン画面でロゴを表示するように変更)

--- src/resources/js/Layouts/GuestLayout.jsx
+++ src/resources/js/Layouts/GuestLayout.jsx
@@ -1,4 +1,4 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
+import logo from '../../assets/images/matrixflow.png';
 import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
@@ -6,7 +6,7 @@ export default function GuestLayout({ children }) {
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                    <img src={logo} alt="ロゴ" className="logo" />
                 </Link>
             </div>
 
