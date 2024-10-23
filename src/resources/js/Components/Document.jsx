843fb59c3cffe4cce5d85864557996860a1e0041
maixhashi <maixhashi@gmail.com>
2024-10-23 23:36:32 +0900

    Feature_2024.10.23.23.35_コンポーネントの修正( Document / fetchFlowstepsWithMembersを不使用化)

--- src/resources/js/Components/Document.jsx
+++ src/resources/js/Components/Document.jsx
@@ -1,38 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchFlowsteps } from '../store/flowstepsSlice'; 
-import axios from 'axios';
 import '../../css/Document.css'
 
 const Document = () => {
   const dispatch = useDispatch();
-
-  const [flowsteps, setFlowsteps] = useState([]);
   
+  // Get flowsteps from Redux store
+  const flowsteps = useSelector((state) => state.flowsteps);
 
   useEffect(() => {
+    // Fetch flowsteps on component mount
     dispatch(fetchFlowsteps());
   }, [dispatch]);
-
-
-  useEffect(() => {
-    const fetchFlowstepsWithMembers = async () => {
-      try {
-        const response = await axios.get('/api/flowsteps'); // すべてのFlowstepを取得するAPI
-        const flowstepsWithMembers = await Promise.all(
-          response.data.map(async (flowstep) => {
-            const res = await axios.get(`/api/flowsteps/${flowstep.id}/members`);
-            return res.data;
-          })
-        );
-        setFlowsteps(flowstepsWithMembers);
-      } catch (error) {
-        console.error('Error fetching flowsteps:', error);
-      }
-    };
-
-    fetchFlowstepsWithMembers();
-  }, []);
 
   return (
     <div className="document-container">
@@ -41,7 +21,7 @@ const Document = () => {
         <div key={flowstep.id} className="chapter">
           <h2 className="chapter">{index + 1}: {flowstep.name}</h2>
           <p>
-            {flowstep.members.length > 0
+            {flowstep.members && flowstep.members.length > 0
               ? `${flowstep.members.map(member => member.name).join(', ')} は${flowstep.name}を行う。`
               : 'Unknown Member が行うタスク:'}
             {flowstep.content}
