203bf4cb7b2decb31eca61278b86a956371146ce
maixhashi <maixhashi@gmail.com>
2024-11-09 20:37:44 +0900

    Feature_2024.11.09.20.37_モーダル閉機能をRedux Slice化(Flowstep追加モーダル)

--- src/resources/js/Components/ModalforAddFlowStepForm.jsx
+++ src/resources/js/Components/ModalforAddFlowStepForm.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { closeAddFlowstepModal } from '../store/modalSlice';
 import '../../css/ModalforAddFlowStepForm.css';
 
 const ModalforAddFlowStepForm = ({ isOpen, onClose, children }) => {
-    if (!isOpen) return null; // モーダルが開いていない場合は何も表示しない
+    const dispatch = useDispatch();
+    
+    const isAddFlowstepModalOpen = useSelector(state => state.modal.isAddFlowstepModalOpen);
+    const handleCloseAddFlowstepModal = (member, selectedStepNumber) => {
+        dispatch(closeAddFlowstepModal(member, selectedStepNumber));
+    };
+    
+    if (!isAddFlowstepModalOpen) return null; // モーダルが開いていない場合は何も表示しない
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <button className="modal-close" onClick={onClose}>×</button>
+                <button className="modal-close" onClick={handleCloseAddFlowstepModal}>×</button>
                 {children}
             </div>
         </div>
