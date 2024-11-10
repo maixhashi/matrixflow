fa537df14cf74cf146085b84b851d0c9ce1d317a
maixhashi <maixhashi@gmail.com>
2024-11-10 16:07:16 +0900

    Style_2024.11.10.16.07_flowstep追加用モーダルのスタイルを変更

--- src/resources/js/Components/AddFlowStepForm.jsx
+++ src/resources/js/Components/AddFlowStepForm.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addFlowstep, deleteFlowstepAsync, fetchFlowsteps } from '../store/flowstepsSlice'; // Adjust the import path as needed
+
+// FontAwesomeのアイコンのインポート
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+
+// スタイル
 import '../../css/AddFlowStepForm.css';
 
 const AddFlowStepForm = ({ members = [], onFlowStepAdded = () => {}, member = null, stepNumber = '', nextStepNumber, workflowId }) => {
@@ -71,63 +77,68 @@ const AddFlowStepForm = ({ members = [], onFlowStepAdded = () => {}, member = nu
     );
 
     return (
-        <div>
-            <form className="form-container" onSubmit={handleSubmit}>
-                <div>
-                    <label>フローステップ名:</label>
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)} 
-                        required
-                        placeholder="担当者がフローとして行うことを入力しましょう"
-                    />
-                </div>
-                <div>
-                    <label>ステップNo.:</label>
-                    <select
-                        value={flowNumber}
-                        onChange={handleStepChange}
-                        required
-                    >
-                        {Array.from({ length: nextStepNumber }, (_, index) => (
-                            <option key={index + 1} value={index + 1}>
-                                STEP {index + 1}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-                <div>
-                    <label>担当者を検索:</label>
-                    <input
-                        type="text"
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                        placeholder="担当者を名前検索できます"
-                    />
-                </div>
-                <div>
-                    <label>担当者を選択:</label>
-                    <select
-                        multiple
-                        value={selectedMembers}
-                        onChange={handleMemberChange}
-                        required
-                    >
-                        {filteredMembers.length > 0 ? (
-                            filteredMembers.map((m) => (
-                                <option key={m.id} value={m.id}>
-                                    {m.name}
+        <>
+            <div className="AddFlowStepForm-title">
+                <FontAwesomeIcon icon={faSquarePlus} className="AddFlowStepForm-title-icon" />
+                フローステップの追加
+                <FontAwesomeIcon icon={faSquarePlus} className="AddFlowStepForm-title-icon" />
+            </div>
+            <div>
+                <form className="form-container" onSubmit={handleSubmit}>
+                    <div>
+                        <label>フローステップ名:</label>
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                            placeholder="担当者がフローとして行うことを入力しましょう" />
+                    </div>
+                    <div>
+                        <label>ステップNo.:</label>
+                        <select
+                            value={flowNumber}
+                            onChange={handleStepChange}
+                            required
+                        >
+                            {Array.from({ length: nextStepNumber }, (_, index) => (
+                                <option key={index + 1} value={index + 1}>
+                                    STEP {index + 1}
                                 </option>
-                            ))
-                        ) : (
-                            <option disabled>No members available</option>
-                        )}
-                    </select>
-                </div>
-                <button type="submit">フローステップを追加</button>
-            </form>
-        </div>
+                            ))}
+                        </select>
+                    </div>
+                    <div>
+                        <label>担当者を検索:</label>
+                        <input
+                            type="text"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            placeholder="担当者を名前検索できます" />
+                    </div>
+                    <div>
+                        <label>担当者を選択:</label>
+                        <select
+                            multiple
+                            value={selectedMembers}
+                            onChange={handleMemberChange}
+                            required
+                        >
+                            {filteredMembers.length > 0 ? (
+                                filteredMembers.map((m) => (
+                                    <option key={m.id} value={m.id}>
+                                        {m.name}
+                                    </option>
+                                ))
+                            ) : (
+                                <option disabled>No members available</option>
+                            )}
+                        </select>
+                    </div>
+                    <button type="submit">フローステップを追加</button>
+                </form>
+            </div>
+        </>
     );
 };
 
